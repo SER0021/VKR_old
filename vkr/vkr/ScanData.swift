6111ce7cf084770fe60ee38ec4d29f8146b58228
SER0021 <ser00021@yandex.ru>
2023-02-10 17:12:03 +0300

    vkr

---
+++ vkr/vkr/ScanData.swift
@@ -0,0 +1,8 @@
+import Foundation
+struct ScanData:Identifiable {
+    var id = UUID()
+    let content:String
+    init(content:String) {
+        self.content = content
+    }
+}
---
+++ vkr/vkr/vkrApp.swift
@@ -0,0 +1,17 @@
+//
+//  vkr1_1App.swift
+//  vkr1.1
+//
+//  Created by MacBook Air on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct vkrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
